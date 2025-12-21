81beff6da3c1fcd9e8393037e941918a274f4ab5
Muhammad Syafiq Fadhilah <145995449+Fadhilsfq@users.noreply.github.com>
2025-12-21 18:42:46 +0700

    Create aes_image.py

---
+++ file sbox/utils/aes_image.py
@@ -0,0 +1,14 @@
+import numpy as np
+from Crypto.Cipher import AES
+
+def encrypt_image(img_array, key):
+    flat = img_array.flatten()
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    pad_len = (16 - len(flat) % 16) % 16
+    flat_padded = np.pad(flat, (0, pad_len), mode='constant')
+
+    encrypted = cipher.encrypt(flat_padded.tobytes())
+    enc_array = np.frombuffer(encrypted, dtype=np.uint8)[:flat.size]
+
+    return enc_array.reshape(img_array.shape)
