87751dd39352b7315454873bc32675e87f72a581
Muhammad Syafiq Fadhilah <145995449+Fadhilsfq@users.noreply.github.com>
2025-12-21 19:49:01 +0700

    Update aes_text.py

--- file sbox/utils/aes_text.py
+++ file sbox/utils/aes_text.py
@@ -5,7 +5,7 @@ import base64
 def format_key(key: str) -> bytes:
     return key.encode().ljust(16, b'\0')[:16]
 
-def encrypt_text(plaintext, key):
+def encrypt_text(plaintext, key, sbox):
     cipher = AES.new(format_key(key), AES.MODE_ECB)
     ct = cipher.encrypt(pad(plaintext.encode(), 16))
     return base64.b64encode(ct).decode()
